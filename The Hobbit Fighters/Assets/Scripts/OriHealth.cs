74cdd8ec8db1e67dbda2906b0b7382db64bf2970
Akash Arora <arora110@mail.chapman.edu>
2018-10-23 19:58:16 -0700

    Health Bar / Combat basics

---
+++ The Hobbit Fighters/Assets/Scripts/OriHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OriHealth : MonoBehaviour {
+	public float CurrentHealth { get; set; }
+	public float MaxHealth { get; set; }
+
+	public Slider healthBar;
+
+	// Use this for initialization
+	void Start () {
+		MaxHealth = 20f;
+		CurrentHealth = MaxHealth;
+		healthBar.value = CalculateHealth();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			DealDamage(2);
+		}
+	}
+
+	void DealDamage(float damageValue) {
+		// Deduce the damage dealt from the character's health
+		CurrentHealth -= damageValue;
+		healthBar.value = CalculateHealth();
+		// If the character is out of health, die!
+		if (CurrentHealth <= 0) {
+			Die();
+		}
+	}
+
+	float CalculateHealth() {
+		return CurrentHealth / MaxHealth;
+	}
+
+	void Die() {
+		CurrentHealth = 0;
+		Debug.Log("you dead");
+	}
+}
---
+++ The Hobbit Fighters/Assets/Scripts/SoundEffectManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectManager : MonoBehaviour {
+
+	public AudioSource bilboPunch;
+	public AudioSource oriStab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.X)) {
+			oriStab.Play();
+		}
+		if (Input.GetMouseButtonDown(0)) {
+			bilboPunch.Play();
+		}
+	}
+}
